14bd5e6c047159a5d10b370bce8a5151104b0ffb
kadir sevil <kadirsevil@MacBook-Pro.local>
2024-07-10 21:30:31 +0300

    removed redundant nproc checking

--- install/index.js
+++ install/index.js
@@ -16,20 +16,9 @@ function execp(command) {
 }
 
 async function install() {
-  let nproc;
-  try {
-    await execp("nproc");
-    nproc = true;
-  } catch (error) {
-    nproc = false;
-  }
-
   await fs.mkdir("third_party/zopfli/build", { recursive: true });
-  await execp(
-    `cd third_party/zopfli/build && cmake .. && make ${
-      nproc ? "-j$(nproc)" : ""
-    }`
-  );
+  await execp("cmake -S third_party/zopfli -B third_party/zopfli/build");
+  await execp(`cmake --build third_party/zopfli/build -j$(nproc)`);
   await execp('npx cmake-js compile -G "Unix Makefiles"');
 }
 
